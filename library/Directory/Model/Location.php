6aabe80421c84463bced6b700044470f91c3ac60
thomaspaulson <thomas.paulson@hotmail.com>
2012-11-12 11:24:23 +0530

    update library folder

--- library/Directory/Model/Location.php
+++ library/Directory/Model/Location.php
@@ -10,7 +10,7 @@
  * @author     ##NAME## <##EMAIL##>
  * @version    SVN: $Id: Builder.php 7490 2010-03-29 19:53:27Z jwage $
  */
-class Directory_Model_Location extends DirectoryIn_Model_BaseLocation
+class Directory_Model_Location extends Directory_Model_BaseLocation
 {
 
 }
